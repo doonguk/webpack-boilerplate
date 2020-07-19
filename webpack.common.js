e5ee0c1cd173dc60485f693d065aa9e2a83ded58
Donguk Hong <mbxd1@naver.com>
2020-07-19 15:49:49 +0900

    feat: style-loader -> MiniCssExtractPlugin 으로 변경. - style loader는 css 파일의 개수만큼 head태그 안에 style 태그가 생김 - MiniCssExtractPlugin은 css 파일들을 한가지 파일로 묶어서 link태그로 import 해줌

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 const config = {
@@ -18,11 +19,14 @@ const config = {
         test: /\.css$/i,
         // use의 경우 오른쪽에서 왼쪽으로 읽습니다.
         // 즉, css-loader를 적용 후 style-loader 적용
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        // css-loader: css 파일을 import 가능, css 파일을 읽어줌
+        // style-loader: 읽은 css 코드를 head 태그안에 넣어줌
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public/index.html"),
       inject: true,
